79692eabc67fe0e75427c2d04a7a1c3a8d44d353
Anna Radlicka <46787289+annaradli@users.noreply.github.com>
2025-01-29 21:53:33 +0100

    Create create-main-workspace.R

---
+++ create-main-workspace.R
@@ -0,0 +1,4 @@
+#the following section created main-workspace.RData:
+source("preprocessing/functions/functions-spatial-data.R")
+source("preprocessing/functions/visualization-functions.R")
+load("~/doktorat aktualne/publikacja preludium/preludium-spatial-new/ldopa-reduced-correct.RData")
